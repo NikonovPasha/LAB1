80503a9bb37e251fff0576f21f2a85b22090cb63
NikonovPasha <77729026+NikonovPasha@users.noreply.github.com>
2021-01-20 18:09:14 +0300

    Add files via upload

---
+++ LAB1.R
@@ -0,0 +1,38 @@
+j < - list(
+  wheels = 4,
+  driverSeat = "Слева",
+  passangersSeats = 3
+)
+class(j) < - "Car"
+print.Car <- function(cr){
+  cat("Колес", j$wheels, "\n")
+  cat("Водительское сидение", j$driverSeat, "\n")
+  cat("Пассажирских сидений", j$passangersSeats, "\n")
+  cat("Название", cr$name, "\n")
+  cat("Топливо", cr$fuel, "\n")
+}
+k < - list(
+  name = "Lada",
+  fuel = "бензин"
+)
+class(k) < - c("NewCar1","Car")
+
+l < - list(
+  name = "BMW",
+  fuel = "дизель"
+)
+class(l) < - c("NewCar2","Car")
+
+m < - list(
+  name = "Tesla",
+  fuel = "электричество"
+)
+class(m) < - c("NewCar3","Car")
+
+zapros < - function(){
+  v1 <- readline("Информацию о какой машине вы хотите получить (Lada, BMW, Tesla)? \n")
+  if (v1 = = "Lada") print.Car(k)
+  if (v1 = = "BMW") print.Car(l)
+  if (v1 = = "Tesla") print.Car(m)
+}
+zapros()
